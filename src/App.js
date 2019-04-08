67b8b838477bbbedc9af3c148eab64c7a5fff47a
Gareth Gomersall <ggomersall@gmail.com>
2019-04-08 11:50:46 +0100

    initial setup of project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="container">
+            <h1 className="logo">
+              <a href="#" >Eclectees<sup>co</sup></a>
+            </h1>
+          </div>
         </header>
       </div>
     );
